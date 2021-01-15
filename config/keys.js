97d352d7bc5c7143033d66650cc18407d9a283f6
Prasanna Hegde <prasannanhegde@gmail.com>
2021-01-15 19:29:45 +0530

    OAuth verification added and connected with mongodb

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+const nodemon = require("nodemon");
+
+// To Choose between developer and production environment
+
+if(process.env.NODE_ENV === 'production') {
+    // We are in production environment
+    module.exports = require('./prod');
+} else {
+    // We are in developer environment
+    module.exports = require('./dev');
+}
