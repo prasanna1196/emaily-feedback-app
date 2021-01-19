33ffae5f7ce44be5c192b22823259defc670d477
Prasanna Hegde <prasannanhegde@gmail.com>
2021-01-19 23:05:33 +0530

    Login logout display on Header

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-  axios.get("/api/current_user");
+export const fetchUser = () => {
+  return async function (dispatch) {
+    const res = await axios.get("/api/current_user");
+    dispatch({ type: FETCH_USER, payload: res.data });
+  };
 };
