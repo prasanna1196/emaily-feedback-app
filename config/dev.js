9c0745bb4eeda6f1d8e7fc96d5dda7384d7d72b0
Prasanna Hegde <prasannanhegde@gmail.com>
2021-01-21 22:46:30 +0530

    stripe payment option added on clientside

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
-    googleClientID: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI: process.env.MONGO_URI,
-    cookieKey: process.env.COOKIE_KEY
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
 };
--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 // require mongoose User model before passport to avoid "model not found" error
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const keys = require('./config/keys');
-require('./models/User');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
 
-
-mongoose.connect(
-    keys.mongoURI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+mongoose
+  .connect(keys.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to DB"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 const app = express();
 
 app.use(
-    cookieSession({
-        maxAge: 30 * 24 * 60 * 60 * 1000,
-        keys: [keys.cookieKey]
-    })
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-require('./routes/authRoutes') (app)
-
-
-
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
